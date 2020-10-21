946c65fb0479a7dcc477b0843f4825d8b54acd2b
Mauro Pfister <mauro.pfister@epfl.ch>
2020-10-21 11:34:21 +0200

    Better handling of input and output

--- plotter.py
+++ plotter.py
@@ -1,4 +1,5 @@
-from matplotlib import pyplot as plt
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 import numpy as np
 import argparse
@@ -9,30 +10,37 @@ from pathlib import Path
 from ffprobe import FFProbe
 from tqdm import tqdm
 import dateutil
-from matplotlib import pyplot as plt
-import matplotlib as mpl
 import datetime
 
 
 class Plotter:
 
-    def __init__(self, video_file, log_file, trail_len=20):
-        self.video_file = video_file
-        self.log_file = log_file
+    def __init__(self, video_file, log_file, intr_calib_file, extr_calib_file, output_file, trail_len=20):
         self.trail_len = trail_len
 
-        self.dir_path = Path(video_file).parent
+        self.cam_mtx, self.dist_coeffs, self.rvec, self.tvec = self.import_cam_calib(extr_calib_file, 
+                                                                                     intr_calib_file,
+                                                                                     video_file)
+        # Input video file
+        self.cap = cv2.VideoCapture(video_file)
+        if not self.cap.isOpened():
+            raise FileNotFoundError(f"{video_file} does not exist or is not a video file.")
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        self.n_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        self.cam_mtx, self.dist_coeffs, self.rvec, self.tvec = self.import_cam_calib(self.dir_path)
-        self.t_start_video = self.get_video_startime(self.video_file)
-        self.cf_pos = self.import_trajectories(self.log_file)
+        self.t_start_video = self.get_video_startime(video_file)
+        self.cf_pos = self.import_trajectories(log_file)
         self.frame_id = 0
         self.t_offset = 0
 
-        # Input video capture
-        self.cap = cv2.VideoCapture(self.video_file)
-        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
-        self.n_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        # Output video file
+        if output_file is None:
+            video_file = Path(video_file)
+            video_file_name = video_file.stem
+            video_file_dir = video_file.parent
+            self.output_file = video_file_dir / f"{video_file_name}_overlay.mp4"
+        else:
+            self.output_file = output_file
 
         # Plotting settings
         dpi = mpl.rcParams['figure.dpi']
@@ -65,16 +73,20 @@ class Plotter:
 
         return cf_pos 
 
-    def import_cam_calib(self, calib_path):
-        extr_calib_path = calib_path / "extrinsic_calib.json" 
-        intr_calib_path = calib_path / "intrinsic_calib.json" 
+    def import_cam_calib(self, extr_calib_file, intr_calib_file, video_file):
+        """Import extrinsic and intrinsic calibrations from json files."""
+        dir_path = Path(video_file).parent
+        if extr_calib_file is None:
+            extr_calib_file = dir_path / "extrinsic_calib.json" 
+        if intr_calib_file is None:
+            intr_calib_file = dir_path / "intrinsic_calib.json" 
 
-        with open(extr_calib_path) as f:
+        with open(extr_calib_file) as f:
             extrinsic_calib = json.load(f)
             rvec = np.array(extrinsic_calib['rvec'])
             tvec = np.array(extrinsic_calib['tvec'])
 
-        with open(intr_calib_path) as f:
+        with open(intr_calib_file) as f:
             intrinsic_calib = json.load(f)
             cam_mtx = np.array(intrinsic_calib['cam_mtx'])
             dist_coeffs = np.array(intrinsic_calib['dist_coeffs'])
@@ -135,7 +147,6 @@ class Plotter:
         self.ax.axis('off')
         self.ax.imshow(img, interpolation='nearest')  # This takes too long...
 
-
         # Handle case where not enough points for full trail length
         if frame_id > trail_len:
             plot_range = np.arange(frame_id - trail_len, frame_id)
@@ -165,16 +176,12 @@ class Plotter:
         self.update()  # Initialize screen with first frame
         while cv2.getWindowProperty(self.window_name, cv2.WND_PROP_VISIBLE) >= 1:
             keyCode = cv2.waitKey(500)
-
             if keyCode == ord('k'):
                 break
-
         cv2.destroyAllWindows()
     
     def save(self):
-        video_out_path = self.dir_path / "output.mp4"
-        out = cv2.VideoWriter(str(video_out_path), cv2.VideoWriter_fourcc(*'mp4v'), self.fps, (self.width, self.height))
-
+        out = cv2.VideoWriter(str(self.output_file), cv2.VideoWriter_fourcc(*'mp4v'), self.fps, (self.width, self.height))
         pbar = tqdm(total=self.n_frames)  # Progress bar
 
         frame_id = 0
@@ -183,18 +190,10 @@ class Plotter:
 
         while self.cap.isOpened():
             ret, frame = self.cap.read()
-
             if not ret:
                 break
-
-            # if frame_id > 200:
-            #     break
-            
             out_frame = self.plot(frame, trajectories, frame_id, self.trail_len)
-
-            # Display the resulting frame
             out.write(out_frame)
-
             frame_id += 1
             pbar.update(1)
 
@@ -207,11 +206,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Plot trajectories over a video')
     parser.add_argument('video_file', type=str, help='Path to the video file')
     parser.add_argument('log_file', type=str, help='Path to log file with trajectories')
+    parser.add_argument('--intrinsic_calib', type=str, help='Path to intrinsic calibration file')
+    parser.add_argument('--extrinsic_calib', type=str, help='Path to extrinsic calibration file')
+    parser.add_argument('--output', type=str, help='Output video file (mp4) with overlaid trajectories')
     args = parser.parse_args()
 
     video_file = args.video_file
     log_file = args.log_file
+    intr_calib_file = args.intrinsic_calib
+    extr_calib_file = args.extrinsic_calib
+    output_file = args.output
 
-    plotter = Plotter(video_file, log_file, trail_len=30)
+    plotter = Plotter(video_file, log_file, intr_calib_file, extr_calib_file, output_file, trail_len=30)
     plotter.run()
     plotter.save()
